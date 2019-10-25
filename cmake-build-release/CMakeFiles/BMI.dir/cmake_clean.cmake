f80f8cbe76b86e57309035ebc1f538adfdf68317
MagnavoxTG <dominik.lohner92@gmx.at>
2019-10-25 12:09:59 +0200

    Version 1.0

---
+++ cmake-build-release/CMakeFiles/BMI.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "BMI"
+  "BMI.pdb"
+  "CMakeFiles/BMI.dir/main.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BMI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
